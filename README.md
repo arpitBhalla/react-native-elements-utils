281b8c27e79f566fefc30e6b5d9d1d840b0669a4
Arpit Bhalla <arpitbhalla2001@gmail.com>
2021-05-31 14:34:29 +0530

    Initial

---
+++ README.md
@@ -0,0 +1,7 @@
+# WatchMan for RNE Demo App
+
+## Installation
+
+```bash
+npx github.com/arpitBhalla/rne.watch -d ../path-to-rne-demo
+```
---
+++ index.js
@@ -0,0 +1,47 @@
+#!/usr/bin/env node
+const chokidar = require("chokidar");
+const chalk = require("chalk");
+const { exec } = require("child_process");
+const { Command } = require("commander");
+const program = new Command();
+program.version("1.0.0");
+
+const watcher = chokidar.watch("./src/**", {
+  ignored: /__tests__/,
+  persistent: true,
+});
+
+program.option("-d, --dir", "output extra debugging");
+
+program.parse(process.argv);
+
+const options = program.opts();
+const demoAppPath = options.dir || "../react-native-elements-app";
+
+console.log();
+const log = (...args) => console.log(chalk.blue.bold("[RNE]", ...args));
+
+log(chalk.blue.bold("React Native Elements"));
+
+watcher
+  .on("ready", () =>
+    log(chalk.italic.yellowBright("Watching for file changes"))
+  )
+  .on("change", (path) => {
+    try {
+      log(chalk.yellow("Change detected at", path, "transpiling & copying..."));
+      exec(
+        `tsc && cp -r ./dist ../${demoAppPath}/node_modules/react-native-elements/`
+      )
+        .on("error", () => console.log("ERROR"))
+        .on("close", () =>
+          log(
+            chalk.green.bold(
+              `Copied to ${demoAppPath}, change a file to re-compile`
+            )
+          )
+        );
+    } catch (error) {
+      log(chalk.red.bold("Error"));
+    }
+  });

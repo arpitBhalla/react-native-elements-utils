087d74fc4a503b5ac6e4181d691ccb95a70001a8
Arpit Bhalla <arpitbhalla2001@gmail.com>
2021-06-04 18:16:06 +0530

    improved

--- packages/rne-codemod/transforms/text-reduce-props.ts
+++ packages/rne-codemod/transforms/text-reduce-props.ts
@@ -1,25 +1,68 @@
-import { Collection, JSCodeshift } from 'jscodeshift';
+import { FileInfo, JSCodeshift } from "jscodeshift";
 
-export default (ast: Collection, j: JSCodeshift) => {
-  const textComponent = ast.findJSXElements('Text');
-  let variantDone = false;
-  ['h1', 'h2', 'h3', 'h4'].forEach((e) => {
-    textComponent
-      .find(j.JSXAttribute, {
-        name: {
-          type: 'JSXIdentifier',
-          name: e,
-        },
-      })
-      .replaceWith((nodePath) => {
-        const { node } = nodePath;
-        if (!variantDone) {
-          node.name.name = 'koko';
-          variantDone = true;
-        }
-        return node;
-      });
+export default function transformer(
+  file: FileInfo,
+  api: { jscodeshift: JSCodeshift }
+) {
+  const j: JSCodeshift = api.jscodeshift;
+
+  let ast = j(file.source);
+
+  const textComponent = ast.findJSXElements("Text");
+
+  textComponent.forEach((nodePath) => {
+    const styleNodePath = j(nodePath).find(j.JSXAttribute, {
+      name: {
+        type: "JSXIdentifier",
+        name: "style",
+      },
+    });
+
+    styleNodePath.replaceWith((childPath) => {
+      const { node } = childPath;
+      console.log("ASDas", JSON.stringify(node.value));
+      node.value = j.jsxExpressionContainer(
+        j.objectExpression(j.objectProperty())
+      );
+
+      return node;
+    });
+
+    ["h1", "h2", "h3", "h4"].forEach((size) => {
+      j(nodePath)
+        .find(j.JSXAttribute, {
+          name: {
+            type: "JSXIdentifier",
+            name: size,
+          },
+        })
+        .replaceWith((childPath) => {
+          const { node } = childPath;
+          node.name = j.jsxIdentifier("variant");
+          node.value = j.stringLiteral(size);
+
+          return node;
+        });
+    });
   });
+  // ['h1', 'h2', 'h3', 'h4'].forEach((e) => {
+  //   textComponent
+  //     .find(j.JSXAttribute, {
+  //       name: {
+  //         type: 'JSXIdentifier',
+  //         name: e,
+  //       },
+  //     })
+  //     .forEach((childNodePath) => {
+  //       j(childNodePath).replaceWith((nodePath) => {
+  //         const { node } = nodePath;
+  //         node.name = j.jsxIdentifier('variant');
+  //         node.value = j.stringLiteral(e);
 
-  return ast;
-};
+  //         return node;
+  //       });
+  //     });
+  // });
+
+  return ast.toSource();
+}

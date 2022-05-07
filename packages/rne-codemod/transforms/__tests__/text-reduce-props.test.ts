1a04fa30da4dc5d17aef3ee48ef6183bff471a60
Arpit Bhalla <55053424+arpitBhalla@users.noreply.github.com>
2022-05-07 18:29:31 +0530

    remove old pkgs

--- packages/rne-codemod/transforms/__tests__/text-reduce-props.test.ts
+++
@@ -1,6 +0,0 @@
-const testHelper = require('../testHelper');
-
-test('card transform test', () => {
-  const { expected, actual } = testHelper('cardTransform', 'cardTransform');
-  expect(expected).toEqual(actual);
-});
